9bec65bf419f4d555f54a0e50d0fe66b8bb2b639
Ivanka1234 <gerasymyuk_az15@nuwm.edu.ua>
2022-10-04 21:36:46 +0200

    pub

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿jQuery('document').ready(function(){
+	jQuery('input').on('keyup',function(){
+		var value1, value2, value3;
+		value1=jQuery('#val1').val();
+		value2=jQuery('#val2').val();
+		value1=Number(value1);
+		value2=Number(value2);
+		jQuery('#rezultat').val(value3);
+	})
+})
