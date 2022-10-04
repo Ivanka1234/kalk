22cc21275d1de3aa146697451e4e310a2c721a16
a.dovhopoliuk@softum.io <kG1321231ILgbyG3I>
2022-10-04 23:35:47 +0300

    my-fix

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-﻿jQuery('document').ready(function(){
-	jQuery('input').on('keyup',function(){
-		var value1, value2, value3;
-		value1=jQuery('#val1').val();
-		value2=jQuery('#val2').val();
-		value1=Number(value1);
-		value2=Number(value2);
-		jQuery('#rezultat').val(value3);
-	})
+﻿jQuery('document').ready(function () {
+    jQuery('input').on('keyup', function () {
+        mathOperation()
+    })
+
+    $('.selector-operator').on('click', function () {
+        jQuery('.selector-operator').removeClass('selected-operator')
+        jQuery(this).addClass('selected-operator')
+        mathOperation()
+    });
 })
+
+function getResult(value1, value2, operator) {
+    if (operator === "+") {
+        return value1 + value2;
+    } else if (operator === "-") {
+        return value1 - value2;
+    } else if (operator === "*") {
+        return value1 * value2;
+    } else if (operator === "/") {
+        return value1 / value2;
+    }
+}
+
+
+function mathOperation() {
+    let value1, value2, operator;
+    value1 = jQuery('.val1').val();
+    value2 = jQuery('.val2').val();
+    operator = jQuery('.selected-operator').text();
+    value1 = Number(value1);
+    value2 = Number(value2);
+    jQuery('.rezultat').text(getResult(value1, value2, operator));
+}
